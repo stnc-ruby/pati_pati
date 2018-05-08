839ac3ff1e24db808f69a0b34abb8f6335215034
Buket Uysal <bkt.uysal@gmail.com>
2018-05-08 17:18:09 +0300

    fixed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :restrict_access
+  # before_action :restrict_access
 
   # APP_KEY = '340a74c924045500d98970a7e65e1c7c'.freeze
 
